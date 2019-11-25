fa10e9c7e5747fb2b13cadf27d15c84fcfd99e11
Eduardo <ceciliorubes@gmail.com>
2019-11-25 17:31:53 +0100

    seeds terminadas

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -6,8 +6,19 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const Location = require("../models/Location");
+const Task = require("../models/Task");
+const Job = require("../models/Job");
 
 const bcryptSalt = 10;
+
+let myLocation = null;
+let boss = null;
+let operator1 = null;
+let operator2 = null;
+let tasks = null;
+let jobs = null;
+let taskArray = null;
 
 mongoose
   .connect("mongodb://localhost/taskrain", { useNewUrlParser: true })
@@ -28,53 +39,16 @@ let users = [
     active: true
   },
   {
-    username: "Jose",
+    username: "Jose Pérez",
     password: bcrypt.hashSync("hello", bcrypt.genSaltSync(bcryptSalt)),
     role: "OPERATOR",
     active: true
-  }
-];
-
-let jobs = [
-  {
-    description: "Cambio de correa de distribución",
-    creator: { type: Schema.Types.ObjectId, ref: "User" },
-    end_date: { type: Date, required: true },
-    start_date: { type: Date, required: true },
-    location: { type: Schema.Types.ObjectId, ref: "Location" },
-    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
   },
   {
-    description: "Cambio de rueda",
-    creator: { type: Schema.Types.ObjectId, ref: "User" },
-    end_date: { type: Date, required: true },
-    start_date: { type: Date, required: true },
-    location: { type: Schema.Types.ObjectId, ref: "Location" },
-    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
-  },
-  {
-    description: "Cambio de aceite",
-    creator: { type: Schema.Types.ObjectId, ref: "User" },
-    end_date: { type: Date, required: true },
-    start_date: { type: Date, required: true },
-    location: { type: Schema.Types.ObjectId, ref: "Location" },
-    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
-  },
-  {
-    description: "Revisión pre-ITV",
-    creator: { type: Schema.Types.ObjectId, ref: "User" },
-    end_date: { type: Date, required: true },
-    start_date: { type: Date, required: true },
-    location: { type: Schema.Types.ObjectId, ref: "Location" },
-    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
-  },
-  {
-    description: "Sustitución de faro",
-    creator: { type: Schema.Types.ObjectId, ref: "User" },
-    end_date: { type: Date, required: true },
-    start_date: { type: Date, required: true },
-    location: { type: Schema.Types.ObjectId, ref: "Location" },
-    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
+    username: "Juan López",
+    password: bcrypt.hashSync("goodbye", bcrypt.genSaltSync(bcryptSalt)),
+    role: "OPERATOR",
+    active: true
   }
 ];
 
@@ -92,9 +66,121 @@ User.deleteMany()
   .catch(err => {
     mongoose.disconnect();
     throw err;
-  })
+  });
 
+let locations = [
+  {
+    location: {
+      type: "Point",
+      coordinates: [40.387493, -3.691193]
+    }
+  }
+];
+
+Location.deleteMany()
   .then(() => {
-    // Close properly the connection to Mongoose
+    return Location.create(locations);
+  })
+  .then(locationsCreated => {
+    console.log(
+      `${locationsCreated.length} locations created with the following id:`
+    );
+    console.log(locationsCreated.map(u => u._id));
+  })
+  .then(() => {
+    console.log("Locations collection created");
+    Location.find()
+      .then(locations => {
+        console.log(locations[0]._id);
+        myLocation = locations[0]._id;
+        return User.find({ role: "BOSS" });
+      })
+      .then(bosses => {
+        boss = bosses[0]._id;
+        return User.find({ role: "OPERATOR" });
+      })
+      .then(operators => {
+        operator1 = operators[0]._id;
+        operator2 = operators[1]._id;
+        Task.deleteMany()
+          .then(() => {
+            tasks = [
+              {
+                name: "Revisión de faros",
+                description:
+                  "Comprobar el estado de los faros encendiendo todas las lámparas",
+                location: myLocation,
+                materials: "llave inglesa",
+                operator: operator1,
+                creator: boss,
+                duration: 15,
+                status: "ONGOING"
+              },
+              {
+                name: "Comprobar el estado de las ruedas",
+                description:
+                  "Comprobación de la profundidad de las marcas y de la presión del neumático",
+                location: myLocation,
+                materials: "regla de medición y manguito con manómetro",
+                operator: operator2,
+                creator: boss,
+                duration: 30,
+                status: "COMPLETED"
+              }
+            ];
+
+            return Task.create(tasks);
+          })
+          .then(tasksCreated => {
+            console.log(
+              `${tasksCreated.length} tasks created with the following id:`
+            );
+            console.log(tasksCreated.map(u => u._id));
+          })
+          .then(() => {
+            console.log("Tasks collection created");
+            Task.find().then(tasks => {
+              taskArray = tasks.map(task => task._id);
+              Job.deleteMany()
+                .then(() => {
+                  jobs = [
+                    {
+                      description: "Pre ITV",
+                      creator: boss,
+                      end_date: "2019-12-02",
+                      start_date: "2019-12-01",
+                      location: myLocation,
+                      tasks: taskArray
+                    }
+                  ];
+                  return Job.create(jobs);
+                })
+                .then(jobsCreated => {
+                  console.log(
+                    `${jobsCreated.length} jobs created with the following id:`
+                  );
+                  console.log(jobsCreated.map(u => u._id));
+                })
+                .then(() => {
+                  console.log("Jobs collection created");
+                })
+                .catch(err => {
+                  mongoose.disconnect();
+                  throw err;
+                })
+                .then(() => {
+                  // Close properly the connection to Mongoose
+                  mongoose.disconnect();
+                });
+            });
+          })
+          .catch(err => {
+            mongoose.disconnect();
+            throw err;
+          });
+      });
+  })
+  .catch(err => {
     mongoose.disconnect();
+    throw err;
   });
