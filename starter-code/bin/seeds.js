6b324be47bff026a11837ab53249585cba9c75c3
Eduardo <ceciliorubes@gmail.com>
2019-11-25 12:59:00 +0100

    seeds empezadas

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -10,38 +10,91 @@ const User = require("../models/User");
 const bcryptSalt = 10;
 
 mongoose
-  .connect('mongodb://localhost/taskrain', {useNewUrlParser: true})
+  .connect("mongodb://localhost/taskrain", { useNewUrlParser: true })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
 let users = [
   {
-    username: "alice",
-    password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
+    username: "Luis Sanchez",
+    password: bcrypt.hashSync("pass", bcrypt.genSaltSync(bcryptSalt)),
+    role: "BOSS",
+    active: true
   },
   {
-    username: "bob",
-    password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
+    username: "Jose",
+    password: bcrypt.hashSync("hello", bcrypt.genSaltSync(bcryptSalt)),
+    role: "OPERATOR",
+    active: true
   }
-]
+];
+
+let jobs = [
+  {
+    description: "Cambio de correa de distribución",
+    creator: { type: Schema.Types.ObjectId, ref: "User" },
+    end_date: { type: Date, required: true },
+    start_date: { type: Date, required: true },
+    location: { type: Schema.Types.ObjectId, ref: "Location" },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
+  },
+  {
+    description: "Cambio de rueda",
+    creator: { type: Schema.Types.ObjectId, ref: "User" },
+    end_date: { type: Date, required: true },
+    start_date: { type: Date, required: true },
+    location: { type: Schema.Types.ObjectId, ref: "Location" },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
+  },
+  {
+    description: "Cambio de aceite",
+    creator: { type: Schema.Types.ObjectId, ref: "User" },
+    end_date: { type: Date, required: true },
+    start_date: { type: Date, required: true },
+    location: { type: Schema.Types.ObjectId, ref: "Location" },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
+  },
+  {
+    description: "Revisión pre-ITV",
+    creator: { type: Schema.Types.ObjectId, ref: "User" },
+    end_date: { type: Date, required: true },
+    start_date: { type: Date, required: true },
+    location: { type: Schema.Types.ObjectId, ref: "Location" },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
+  },
+  {
+    description: "Sustitución de faro",
+    creator: { type: Schema.Types.ObjectId, ref: "User" },
+    end_date: { type: Date, required: true },
+    start_date: { type: Date, required: true },
+    location: { type: Schema.Types.ObjectId, ref: "Location" },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
+  }
+];
 
 User.deleteMany()
-.then(() => {
-  return User.create(users)
-})
-.then(usersCreated => {
-  console.log(`${usersCreated.length} users created with the following id:`);
-  console.log(usersCreated.map(u => u._id));
-})
-.then(() => {
-  // Close properly the connection to Mongoose
-  mongoose.disconnect()
-})
-.catch(err => {
-  mongoose.disconnect()
-  throw err
-})
+  .then(() => {
+    return User.create(users);
+  })
+  .then(usersCreated => {
+    console.log(`${usersCreated.length} users created with the following id:`);
+    console.log(usersCreated.map(u => u._id));
+  })
+  .then(() => {
+    console.log("Users collection created");
+  })
+  .catch(err => {
+    mongoose.disconnect();
+    throw err;
+  })
+
+  .then(() => {
+    // Close properly the connection to Mongoose
+    mongoose.disconnect();
+  });
